85ddbe573d5ea1f13d1ca89fc952a47d52a5fbd2
Faramarz Alizadh <framarzalizadh7@gmail.com>
2023-06-11 11:41:34 +0430

    Hello

---
+++ app.js
@@ -0,0 +1,23 @@
+const larg_balance=document.getElementById("balance");
+const income=document.getElementById("income");
+const expense=document.getElementById("expense");
+const area=document.getElementById("area");
+const text=document.getElementById("text");
+const amount=document.getElementById("amount");
+const button=document.getElementById("submit");
+console.log(larg_balance, income ,expense, area, amount ,text, submit);
+
+// Adding event section
+
+button.addEventListener("click" , inseart);
+
+
+
+// Function section
+
+function inseart(){
+
+    const value=text.value;
+    
+
+}
