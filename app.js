151cf90794d550f4374e126618f54fb11f3d3e22
Faramarz-Saidmohammadi <136171600+Faramarz-Saidmohammadi@users.noreply.github.com>
2024-05-13 11:39:57 +0430

    This project is focus on localstorage of the browser
    This project is made by HTML, CSS, JavaScript that you book the  sets like a real cinema booking.

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-const larg_balance=document.getElementById("balance");
-const income=document.getElementById("income");
-const expense=document.getElementById("expense");
-const area=document.getElementById("area");
-const text=document.getElementById("text");
-const amount=document.getElementById("amount");
-const button=document.getElementById("submit");
-console.log(larg_balance, income ,expense, area, amount ,text, submit);
+const movie = document.getElementById("movie");
+let ticketPrice = +movie.value;
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const clear = document.getElementById("clear");
 
-// Adding event section
+readDB();
 
-button.addEventListener("click" , inseart);
+// Container event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateCountandTotal();
+  }
+});
 
+movie.addEventListener("change", (e) => {
+  ticketPrice = +movie.value;
+  updateCountandTotal();
+});
 
+// update Count and total
+function updateCountandTotal() {
+  const selectedSeat = document.querySelectorAll(".row .seat.selected");
+  const selectedIndex = [...selectedSeat].map((seat) =>
+    [...seats].indexOf(seat)
+  );
+  localStorage.setItem("selectedSeats", JSON.stringify(selectedIndex));
 
-// Function section
-
-function inseart(){
-
-    const value=text.value;
-    
-
+  const selectedSeatCount = selectedSeat.length;
+  count.innerHTML = selectedSeatCount;
+  total.innerHTML = selectedSeatCount * ticketPrice;
 }
+
+// readDb
+function readDB() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("occupied");
+      }
+    });
+  }
+}
+
+//
+clear.addEventListener("click", () => {
+  localStorage.clear();
+});
